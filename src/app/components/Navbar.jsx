0618d7126869eb56b15bb0fecf7b1f6bd80d087d
varjain <varjain@redhat.com>
2024-07-15 19:02:13 +0530

    add gradient to navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className=' py-2 flex flex-wrap items-center justify-between mx-auto px-4 bg-[#BC6C25] text-white fixed top-0 left-0 right-0'>
+    <div className='bg-gradient-to-b from-[#bc6c25] to-[#dda15e] py-2 flex flex-wrap items-center justify-between mx-auto px-7  text-white fixed top-0 left-0 right-0'>
         <div className='text-lg font-semibold tracking-wide'>
             Vardhman
         </div>
-        <ul className='justify-end p-0 text-xs ml-auto'>
+        <ul className='justify-end p-0 text-sm sm:text-xs ml-auto font-mediumz'>
             <li className='inline ml-3 hover:border-b-2'><Link href="#">About</Link></li>
             <li className='inline ml-3 hover:border-b-2'><Link href="#">Blog</Link></li>
             <li className='inline ml-3 hover:border-b-2'><Link href="#projects">Projects</Link></li>
