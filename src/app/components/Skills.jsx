c1ba40356872e874ad3cf70698cbb1728784534c
varjain <varjain@redhat.com>
2024-07-19 20:36:38 +0530

    commit

--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -6,10 +6,10 @@ function Skills() {
   return (
     <div className='flex flex-col items-center'>
         <div className='sub-heading'>Skills</div>
-        <div className='flex flex-wrap flex-row justify-center sm:text-sm sm:max-w-[30rem] '>
+        <div className='flex flex-wrap flex-row justify-center text-xs sm:max-w-[30rem]'>
             {
                 skills.map((skill, index) =>{
-                    return (<div key={index} className='px-2 py-1 bg-[#FEFAE0] rounded-full m-2 shadow-lg'>
+                    return (<div key={index} className='px-2 py-1 border-[#bc6c25] border-[1px] rounded-full m-2 shadow-sm'>
                         {skill}
                     </div>)
                 })
