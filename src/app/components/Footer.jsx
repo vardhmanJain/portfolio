ce32905f67df3b46c3dd87d5bb7f790616a1dfd0
varjain <varjain@redhat.com>
2024-07-19 20:19:47 +0530

    fixes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='min-h-32 bg-[#283618] w-[100%] mt-16'>
-        Footer
+    <div className='min-h-20 text-sm bg-[#bc6c25] w-[100%] mt-16 flex flex-row items-center justify-center text-[#fefae0]'>
+        <div>&#169; 2024 Vardhman Jain</div>
     </div>
   )
 }
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='bg-gradient-to-b from-[#bc6c25] to-[#dda15e] py-2 flex flex-wrap items-center justify-between mx-auto px-4 sm:px-5 text-white fixed top-0 left-0 right-0'>
-        <div className='text-lg font-semibold tracking-wide'>
-            Vardhman
+    <div className='py-2 flex flex-wrap items-center justify-between mx-auto px-4 sm:px-5  fixed top-0 left-0 right-0'>
+        <div className='text-base tracking-wide border-b-2 border-[#bc6c25]'>
+            <Link href="/">vardhman.dev</Link>
         </div>
-        <ul className='justify-end p-0 text-sm sm:text-xs ml-auto font-mediumz'>
-            <li className='inline ml-3 hover:border-b-2'><Link href="#">About</Link></li>
-            <li className='inline ml-3 hover:border-b-2'><Link href="#">Blog</Link></li>
-            <li className='inline ml-3 hover:border-b-2'><Link href="#projects">Projects</Link></li>
+        <ul className='justify-end p-0 text-sm sm:text-xs ml-auto '>
+            <li className='inline ml-3 hover:border-b-2 hover:border-[#bc6c25]'><Link href="#">About</Link></li>
+            <li className='inline ml-3 hover:border-b-2 hover:border-[#bc6c25]'><Link href="https://vardhman.hashnode.dev">Blog</Link></li>
+            <li className='inline ml-3 hover:border-b-2 hover:border-[#bc6c25]'><Link href="#projects">Projects</Link></li>
         </ul>
     </div>
   )
